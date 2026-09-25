35840ae5b51959a5644a8e1fdf98f1052c201af1
MURAMATSU Atsushi <amura@tomato.sakura.ne.jp>
2026-09-25 10:47:24 +0900

    fix date num

--- init.lua
+++ init.lua
@@ -10,9 +10,9 @@ function Linemode:size_and_mtime()
   if time == 0 then
     time = ""
   elseif os.date("%Y", time) == os.date("%Y") then
-    time = os.date("%b%d %H:%M", time)
+    time = os.date("%m%dT%H%M", time)
   else
-    time = os.date("%Y-%b%d", time)
+    time = os.date(" %Y%m%d", time)
   end
   
   local size = self._file:size()
